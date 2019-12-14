e1ef59508ce777c30581c023cac151cfe3cd6b5a
MinexAutomation <minex.automation@gmail.com>
2019-12-14 02:03:03 -0800

    Added FileNames and IConfigurationBuilder to add the GitHub authentications secrets file.

---
+++ source/R5T.Polidea.Suebia/Code/Classes/FileNames.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Polidea.Suebia
+{
+    public static class FileNames
+    {
+        public const string GitHubAuthenticationJsonFileName = "Authentication-GitHub.json";
+    }
+}
---
+++ source/R5T.Polidea.Suebia/Code/Extensions/IConfigurationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+
+using R5T.Suebia.Extensions;
+
+
+namespace R5T.Polidea.Suebia.Code.Extensions
+{
+    public static class IConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder AddGitHubAuthenticationConfiguration(this IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider)
+        {
+            configurationBuilder.AddJsonSecretsFilePath(configurationServiceProvider, FileNames.GitHubAuthenticationJsonFileName);
+
+            return configurationBuilder;
+        }
+    }
+}
